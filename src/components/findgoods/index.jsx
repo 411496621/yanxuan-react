08d3687dd99e77808c3e7127325d2bb3c9c6be5f
411496621 <411496621@qq.com>
2019-01-07 19:32:23 +0800

    识物功能进行中

--- src/components/findgoods/index.jsx
+++ src/components/findgoods/index.jsx
@@ -1,12 +1,22 @@
 import React, {Component} from 'react'
 import Footer from "../footer"
 class FindGoods extends Component {
+
+
   render() {
     return (
-      <div className='atguigu'>
-        findgoods
+      <div className="find-goods">
+        <div  className='find-goods-header' >
+          <div className='left'>
+            <i className="iconfont icon-shouye"></i>
+          </div>
+
+          <div className="right">
+            <i className="iconfont icon-gouwuche"></i>
+          </div>
+        </div>
         <Footer />
-      </div>
+     </div>
     )
   }
 }
