f5522833e6940e76128b7c5aa31a000bcb1b265c
411496621 <411496621@qq.com>
2019-01-07 11:34:19 +0800

    配置好stylus

---
+++ src/components/app/index.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import "./index.styl"
+class App extends Component {
+  render() {
+    return (
+      <div>
+        xxx
+      </div>
+    )
+  }
+}
+
+export default App
